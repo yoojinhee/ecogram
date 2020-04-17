5d7ceeedab75092533c0f02cc0e2b3ddf4a31187
yoojinhee <52348335+yhujinhui@users.noreply.github.com>
2020-04-18 03:22:41 +0900

    메인화면 구현

---
+++ ecostagram/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+	<meta charset="UTF-8">
+	<title>ecostagram</title>
+	<style>
+		@import url("css/index.css");
+	</style>
+</head>
+<body>
+	<div class="container">
+		<header>
+			<span><a href="" id="log">로그인</a></span>
+			<div></div>
+		</header>
+		<nav>
+			<span class="nav-span"><a href=""><img src="images/test.png" alt="로고" height="50" width="50" id="logo"></a></span>
+			<span>
+				<a href="">이벤트</a>
+				<a href="">피드</a>
+				<a href="">사진 업로드</a>
+				<a href="">게시물</a>
+			</span>
+		</nav>
+		<main>
+			<div class="content">
+				<div id="text">#Save the Earth #love Earth</div>
+				<div><img src="images/test.png" alt="이미지1" width="800" height="533"></div>
+				<span><a href="" id="prev">prev</a></span>
+				<span><a href="" id="next">next</a></span>
+			</div>
+		</main>
+	</div>
+</body>
+</html>
