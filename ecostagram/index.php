f679a515a0667b2df0ebbd4a2183ba2d37285a92
yoojinhee <52348335+yhujinhui@users.noreply.github.com>
2020-04-19 04:30:15 +0900

    이미지 슬라이드 구현

--- ecostagram/index.php
+++ ecostagram/index.php
@@ -5,9 +5,12 @@
 	<title>ecostagram</title>
 	<style>
 		@import url("css/index.css");
+		@import url("css/jquery.bxslider.min.css");
+
 	</style>
+  <script src="js/index.js"></script>
 </head>
-<body>
+<body onload="javascript:showSlides();">
 	<div class="container">
 		<header>
 			<span><a href="" id="log">로그인</a></span>
@@ -25,10 +28,36 @@
 		<main>
 			<div class="content">
 				<div id="text">#Save the Earth #love Earth</div>
-				<div><img src="images/test.png" alt="이미지1" width="800" height="533"></div>
+				<div class="slideshow-container">
+
+					<div class="mySlides fade">
+					  <!-- <div class="numbertext">1 / 3</div> -->
+					  <img src="images/test.png" style="width:100%">
+					  <!-- <div class="text">Caption One</div> -->
+					</div>
+
+					<div class="mySlides fade">
+					  <!-- <div class="numbertext">2 / 3</div> -->
+					  <img src="images/test.png" style="width:100%">
+					  <!-- <div class="text">Caption Two</div> -->
+					</div>
+
+					<div class="mySlides fade">
+					  <!-- <div class="numbertext">3 / 3</div> -->
+					  <img src="images/test.png" style="width:100%">
+					  <!-- <div class="text">Caption Three</div> -->
+					</div>
+					</div>
+					<br>
+					<div style="text-align:center">
+					  <span class="dot"></span> 
+					  <span class="dot"></span> 
+					  <span class="dot"></span> 
+					</div>
+				<!-- <div><img src="images/test.png" alt="이미지1" width="800" height="533"></div>
 				<span><a href="" id="prev">prev</a></span>
-				<span><a href="" id="next">next</a></span>
-			</div>
+				<span><a href="" id="next">next</a></span> -->
+			
 		</main>
 	</div>
 </body>
