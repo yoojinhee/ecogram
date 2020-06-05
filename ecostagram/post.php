403e0dd0d4566c3444a9e4178aa8e14b0655fa4f
yoojinhee <52348335+yhujinhui@users.noreply.github.com>
2020-06-05 23:43:34 +0900

    게시물 css

---
+++ ecostagram/post.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+	<meta charset="UTF-8">
+	<title>ecostagram : 게시물</title>
+	<style>
+		@import url("css/post.css");
+		@import url("css/header.css");
+	</style>
+</head>
+<body>
+	<?php 
+		require_once("lib/header_bottom.php");
+	?>
+	<div class="wrraper">
+		<div class="top">
+			<div class="post">
+				<a href="">게시물</a>
+			</div>
+			<div class="upload">
+				<a href="">업로드</a>
+			</div>
+		</div>
+		<div class="container">
+			<div class="row">
+				<span class="items">
+					<img src="images/test.png" width="326" height="300" alt="테스트이미지">
+				</span>
+				<span class="items">
+					<img src="images/test.png" width="326" height="300" alt="테스트이미지">
+				</span>
+				<span class="items">
+					<img src="images/test.png" width="326" height="300" alt="테스트이미지">
+				</span>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
