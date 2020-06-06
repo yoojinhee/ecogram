f9cce487e122ecfbad8994cf97ef1427a199b3be
yoojinhee <52348335+yhujinhui@users.noreply.github.com>
2020-06-06 17:07:26 +0900

    로그인 js 수정

--- ecostagram/js/header.js
+++ ecostagram/js/header.js
@@ -10,9 +10,7 @@ function logoutchk(){
 
 function loginchk(){
 	if(confirm("로그인이 필요합니다. 로그인 하시겠습니까?")){
-		location.href = "login.html";
-		return true;
-	} else {
-    	return false;
+		location.href = "login.php";
 	}
+    return false;
 }//로그아웃 체크
