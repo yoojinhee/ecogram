6905c8addd271ab908358d11fc42ba5c078e463b
dayeon1021 <48789415+dayeon1021@users.noreply.github.com>
2020-06-06 18:41:53 +0900

    피드 폼 수정

--- ecostagram/feed.php
+++ ecostagram/feed.php
@@ -1,8 +1,8 @@
 <?php  
 $url="feed.php";
 require_once("lib/header_top.php");
-$sql="select * from feed order by _id desc";
-$result=mysqli_query($conn,$sql);
+// $sql="select * from feed order by _id desc";
+// $result=mysqli_query($conn,$sql);
 
 require_once("lib/show_post_top.php");
 ?>
@@ -26,11 +26,9 @@ require_once("lib/show_post_top.php");
 	?>
 	<div class="wrapper">
 		<div class="top">
-			<div class="search">
-				<input type="text" name="search" class="searchtext" placeholder="검색">
-			</div>
+			<form methos="get" name="frm1" action='feed.php'>
 			<div class="select">
-				<select name="" id="" class="selectbox">
+				<select name="kind" class="selectbox">
 					<option value="">지역</option>
 					<option value="서울">서울</option>
 					<option value="부산">부산</option>
@@ -38,12 +36,17 @@ require_once("lib/show_post_top.php");
 
 				</select>
 			</div>
+			<div class="search">
+				<input type="text" name="search" class="searchtext" placeholder="검색">
+				<input type="button" name=byn1 onclick="search1()" value="찾기">
+			</div>
+			</form>
+			
 		</div>
 		<div class="container">
 			<?=$list?>
 		</div>
 	</div>
 
-	
 </body>
 </html>
