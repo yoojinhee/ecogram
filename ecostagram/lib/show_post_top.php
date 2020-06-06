4d5d5ff53448529c0582ef87f5dc1f9bba6a4840
dayeon1021 <48789415+dayeon1021@users.noreply.github.com>
2020-06-06 18:42:35 +0900

    피드 검색 기능 추가

--- ecostagram/lib/show_post_top.php
+++ ecostagram/lib/show_post_top.php
@@ -1,14 +1,49 @@
 <?php 
 
+
+if(isset($_GET['search'])){
+    $sel=$_GET['kind'];
+    $search=$_GET['search'];
+
+	if(empty($sel)){
+		echo
+		'<script>
+		alert("지역을 선택해주세요");
+		location.href="feed.php";
+		</script>';
+	}else{
+		$sql="select * from feed where title like '%$search%' && area='$sel' order by _id desc";
+		$result=mysqli_query($conn,$sql);
+	
+		$result_count=mysqli_query($conn, "select count(*) from feed where title like '%$search%' && area='$sel' order by _id desc");
+
+		if(!empty($result_count)){
+			echo
+			'<script>
+			alert("검색 결과가 없습니다");
+			location.href="feed.php";
+			</script>';
+		}else{
+			
+		}
+	}
+
+}else{
+    $sql="select * from feed order by _id desc";
+	$result=mysqli_query($conn,$sql);
+
+    //$result_count=mysqli_query($conn, "select count(*) from feed");
+}
+
 $list='';
 
 while($row=mysqli_fetch_array($result)){
-	$list=$list.
-	'
-	<span class="items">
-		<a href="'.$url.'?id='.$row['_id'].'" onclick="clickpost()"><img src="uploads/'.$row['file'].'" width="326" height="300" alt="테스트이미지"></a>
-	</span>
-	 ';
+		$list=$list.
+		'
+		<span class="items">
+			<a href="'.$url.'?id='.$row['_id'].'" onclick="clickpost()"><img src="uploads/'.$row['file'].'" width="326" height="300" alt="테스트이미지"></a>
+		</span>
+		 ';
 }
 
 $IsGETid=0;
